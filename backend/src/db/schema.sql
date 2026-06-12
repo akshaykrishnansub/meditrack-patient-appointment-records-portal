d3543583d407170c377f688d9fe655fd9acacf6e
unknown <akshaykrish533516@gmail.com>
2026-06-12 15:59:58 +0530

    Initial tables created

---
+++ backend/src/db/schema.sql
@@ -0,0 +1,39 @@
+create database meditrack;
+
+create TYPE user_role AS ENUM(
+'PATIENT',
+'DOCTOR',
+'ADMIN'
+)
+
+create table users(
+id uuid primary key default gen_random_uuid(),
+name varchar(255) not null,
+email varchar(255) unique not null,
+password_hash TEXT not null,
+role user_role not null
+);
+
+create table appointment(
+id uuid primary key default gen_random_uuid(),
+patientId uuid references users(id),
+doctorId uuid references users(id),
+datetime timestamp not null,
+status varchar(50) not null
+);
+
+create table medicalrecord(
+id uuid primary key default gen_random_uuid(),
+userId uuid references users(id),
+filePath text not null,
+uploadedAt timestamp not null,
+description text not null
+);
+
+create table message(
+id uuid primary key default gen_random_uuid(),
+senderId uuid references users(id),
+receiverId uuid references users(id),
+content text not null,
+sentAt timestamp not null
+);
