c3d9b4fb2cd49957f18098a36c997cc6fc78db77
unknown <akshaykrish533516@gmail.com>
2026-06-14 08:25:31 +0530

    Login API implemented

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -6,7 +6,7 @@ import jwt from 'jsonwebtoken'
 export const registerUser=async(req:Request,res:Response)=>{
 
     //Destructuring
-    const {name,email,password_hash,role} = req.body;
+    const {name,email,password,role} = req.body;
     try{
         //Check if user already exists
         const existingUser=await findUserByEmail(email);
@@ -15,13 +15,13 @@ export const registerUser=async(req:Request,res:Response)=>{
         }
 
         //Hashing password
-        const hashedPassword=await bcrypt.hash(password_hash,10);
+        const hashedPassword=await bcrypt.hash(password,10);
 
         //Creating new user
         const newUser=await createUser(name,email,hashedPassword,role);
 
         //Generating Token
-        const token=jwt.sign({id:newUser.id},process.env.JWT_TOKEN!,{expiresIn:'1d'})
+        const token=jwt.sign({id:newUser.id},process.env.JWT_SECRET!,{expiresIn:'1d'})
 
         //return the success message
         return res.status(201).json({message:'User Registered successfully'});
@@ -29,5 +29,40 @@ export const registerUser=async(req:Request,res:Response)=>{
     }catch(err){
         console.error(err);
         res.status(500).json({error:'Internal Server Error'})
+    }
+}
+
+export const login=async(req:Request,res:Response)=>{
+    const {email,password}=req.body;
+    try{
+        //find the user by email
+        const user=await findUserByEmail(email);
+
+        //check if user exists
+        if(!user){
+            return res.status(400).json({error:'User not found, Please try registering and sign in again'});
+        }
+        //compare the passwords
+        const matchedPassword=await bcrypt.compare(password,user.password_hash);
+        if(!matchedPassword){
+            return res.status(401).json({error:"Invalid Credentials"});
+        }
+
+        //generate token
+        const token=jwt.sign({id:user.id},process.env.JWT_SECRET!,{expiresIn:'1d'});
+
+        //store the token as a cookie
+        res.cookie('token',token,{
+            httpOnly:true,
+            secure:true,
+            sameSite:"none",
+            maxAge:1*24*60*60*1000
+        })
+
+        return res.json({message:"Login Successful",token,user:{id:user.id,name:user.name}})
+
+    }catch(err){
+        console.error(err);
+        res.status(500).json({error:'Internal Server Error'});
     }
 }
--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/auth.controller.js";
+import { login, registerUser } from "../controllers/auth.controller.js";
 
 const router=Router();
 
 router.post("/register",registerUser)
+router.post("/login",login)
 
 export default router;
