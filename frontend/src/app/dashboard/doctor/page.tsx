6ad0f88c3057f155a9a247f4d0992b6f59392c13
unknown <akshaykrish533516@gmail.com>
2026-06-25 15:31:11 +0530

    Responsiveness Added

--- frontend/src/app/dashboard/doctor/page.tsx
+++ frontend/src/app/dashboard/doctor/page.tsx
@@ -6,7 +6,7 @@ const DoctorDashboard = () => {
   return (
     <div className='bg-gray-100 flex'>
       {/*sidebar*/}
-      <aside className='min-h-screen w-64 bg-white shadow-md p-6'>
+      <aside className='hidden lg:block min-h-screen w-64 bg-white shadow-md p-6'>
         <h1 className='text-2xl font-bold mb-8'>Medi<span className='text-green-600'>Track</span></h1>
         <nav className='space-y-4'>
           <Link href="/dashboard/doctor" className='block font-bold p-2 rounded hover:bg-green-200'>Doctor Dashboard</Link>
