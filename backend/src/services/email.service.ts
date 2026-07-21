6e34de1b784464c63812bb96137255b4972e2cf2
unknown <akshaykrish533516@gmail.com>
2026-07-21 16:14:04 +0530

    Modified email service code for production

--- backend/src/services/email.service.ts
+++ backend/src/services/email.service.ts
@@ -9,7 +9,9 @@ import { doctorWelcomeTemplate } from '../templates/doctorWelcome.js';
 
 console.log("Attempting to send email...");
 const transporter=nodemailer.createTransport({
-    service:"gmail",
+    host:"smtp.gmail.com",
+    port:587,
+    secure:false,
     auth:{
         user:process.env.EMAIL_USER,
         pass:process.env.EMAIL_PASS
